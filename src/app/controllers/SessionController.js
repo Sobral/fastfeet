4ddf195cfc499453efd73a51f0b8ad93cdb5351f
Luciano Sobral <sobral.luciano@gmail.com>
2020-02-04 13:02:14 -0400

    Adds sessioncontroller

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,48 @@
+import jwt from 'jsonwebtoken';
+import Yup from 'yup';
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    const schemaValid = await schema.isValid(request.body);
+
+    if (!schemaValid) {
+      return response.status(400).json({ error: 'Validation Fails!' });
+    }
+    const { email, password } = request.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return response.status(401).json({ error: 'User not found.' });
+    }
+
+    const userSessionAuthorized = await user.checkPassword(password);
+
+    if (!userSessionAuthorized) {
+      return response.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = user;
+    const { secretKey, expiresIn } = authConfig;
+    const token = jwt.sign({ id }, secretKey, { expiresIn });
+    return response.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token,
+    });
+  }
+}
+
+export default new SessionController();
