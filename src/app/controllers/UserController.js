e3ae11dd44fe5cc30b82865b854cb3633ec70075
Luciano Sobral <sobral.luciano@gmail.com>
2020-02-03 23:24:54 -0400

    adds GET /users/:id

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,6 +8,21 @@ class UserController {
 
     return response.status(200).json(users);
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const user = await User.findOne({
+      where: { id },
+      attributes: ['id', 'name', 'email', 'admin'],
+    });
+
+    if (!user) {
+      return response.status(401).json({ error: 'User not found!' });
+    }
+
+    return response.status(200).json(user);
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ import UserController from './app/controllers/UserController';
 const routes = new Router();
 
 routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
 
 export default routes;
