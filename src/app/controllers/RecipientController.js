28b34893bd75f67875a3220bc61e59f025fb045c
Luciano Sobral <sobral.luciano@gmail.com>
2020-02-05 15:42:18 -0400

    Fix('model recipient') Recipient is created properly
    When we sent a json from route /recipient as post method, the app
    was crashing
    
    fix crash when creating a recipient

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,10 +1,28 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.string().required(),
+      cep: Yup.string().required(),
+      cidade: Yup.string().required(),
+      estado: Yup.string().required(),
+      complemento: Yup.string().required(),
+    });
+
+    const schemaValid = await schema.isValid(request.body);
+
+    if (!schemaValid) {
+      return response.status(400).json({ error: 'Fail to validade request.' });
+    }
+
     const recipient = await Recipient.create(request.body);
 
-    return response.json(recipients);
+    return response.json(recipient);
   }
 }
 
